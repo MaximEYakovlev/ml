6e428236ac4cacca115f928396b31c52b22d456c
Maxim Yakovlev <it.trainee@yandex.ru>
2025-05-11 03:26:14 +0300

    defined a model for linear regression

--- TensorFlow.js
+++ TensorFlow.js
@@ -1,1 +1,7 @@
 import * as tf from '@tensorflow/tfjs';
+
+// Define a model for linear regression.
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
