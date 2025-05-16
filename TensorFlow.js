64b865653bd4a35f27813752af3fbba39a09cd6b
Maxim Yakovlev <it.trainee@yandex.ru>
2025-05-17 02:49:03 +0300

    trained the model

--- TensorFlow.js
+++ TensorFlow.js
@@ -9,3 +9,12 @@ model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
 
 const xs = tf.randomNormal([100, 10]);
 const ys = tf.randomNormal([100, 1]);
+
+// Train the model.
+model.fit(xs, ys, {
+  epochs: 100,
+  callbacks: {
+    onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+  }
+});
+
