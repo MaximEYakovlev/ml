e3b51879ea91c5371f89370352b4054417e5541a
Maxim Yakovlev <it.trainee@yandex.ru>
2025-05-16 04:06:51 +0300

    defined a model

--- TensorFlow.js
+++ TensorFlow.js
@@ -1,2 +1,11 @@
 // Use `tfjs-node`. Note that `tfjs` is imported indirectly by `tfjs-node`.
 const tf = require('@tensorflow/tfjs-node');
+
+// Define a simple model.
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
+model.add(tf.layers.dense({units: 1, activation: 'linear'}));
+model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
